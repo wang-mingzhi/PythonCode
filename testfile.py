3147f73484b98a06c1d8f151e1283d16f8b2c582
18120900 <1765471602@qq.com>
2020-07-26 09:53:34 +0800

    添加高考一分一段表分析程序

--- testfile.py
+++ testfile.py
@@ -5,9 +5,10 @@
 # Software: PyCharm
 # Describe:
 import time
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-@profile
 def fun():
     a = 0
     b = 0
@@ -19,5 +20,24 @@ def fun():
     return a + b
 
 
+def dealdata():
+    data = pd.read_excel(r"F:\18120900\桌面\河南高考一分一段表-2020.xlsx")
+    score = dict(zip(data.iloc[::2, 0], data.iloc[1::2, 0]))
+    df = pd.DataFrame.from_dict(score, orient='index', columns=['count'])
+    df = df.reset_index().rename(columns={'index': 'score'})
+    df.sort_values(['score'], ascending=False, inplace=True)
+    df['diff'] = df['count'].diff(1)
+    df.iloc[0, 2] = df.iloc[0, 1]
+    df.to_excel('河南高考一分一段表-2020.xlsx', index=None)
+
+
 if __name__ == "__main__":
-    fun()
+    # fun()
+    # dealdata()
+    sheet = pd.read_excel('河南高考一分一段表-2020.xlsx')
+    fig, axes = plt.subplots(2, 1)
+    axes[0].plot(sheet['score'], sheet['count'])
+    axes[1].plot(sheet['score'], sheet['diff'])
+    plt.tight_layout()
+    plt.show()
+    print('Done!')
